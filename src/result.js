d0305126d4155728234f2006e4449253d95f8d4c
moti <moti.kor@gmail.com>
2021-05-16 10:06:01 +0200

    langs

--- src/result.js
+++ src/result.js
@@ -1,44 +1,41 @@
 /* globals Tesseract */
+{
+  let worker, workerReady;
+  const initWorker = async () => {
+    worker = Tesseract.createWorker({
+      workerPath: chrome.runtime.getURL("lib/tesseract/worker.min.js"),
+      // langPath: chrome.runtime.getURL("traineddata"),
+      corePath: chrome.runtime.getURL("lib/tesseract/tesseract-core.wasm.js"),
+      logger: (m) => console.log("~~~", m),
+    });
+    const lang = "eng+heb";
+    await worker.load();
+    await worker.loadLanguage(lang);
+    await worker.initialize(lang);
+    workerReady = true;
+  };
 
-const doOCR = async (base64) => {
-  const worker = new Tesseract.TesseractWorker({
-    workerPath: chrome.runtime.getURL("lib/tesseract/worker.min.js"),
-    // langPath: chrome.runtime.getURL("traineddata"),
-    corePath: chrome.runtime.getURL("lib/tesseract/tesseract-core.wasm.js"),
-  });
+  const doOCR = async (base64) => {
+    if (!workerReady) await initWorker();
+    const { data } = await worker.recognize(base64);
+    console.log(data);
+    if (data.text?.trim?.() === "") {
+      console.warn("No text was detected");
+    } else {
+      window.open(
+        `https://translate.google.com/?hl=en#auto/en/${data.text}`,
+        "Google Translate",
+        "height=400,width=776,location=0,menubar=0,scrollbars=1,toolbar=0"
+      );
+    }
+  };
 
-  worker
-    .recognize(base64)
-    .progress((report) => {
-      if (report.status === "recognizing text") {
-        console.log(report.progress);
-      } else if (report.status === "loaded language traineddata") {
-        console.log(report.progress);
-      }
-    })
-    .then(
-      (data) => {
-        console.log(data);
-        console.log(data.text);
+  const initPage = async () => {
+    initWorker();
+    window.addEventListener("message", (e) => {
+      doOCR(e.data.dataUrl);
+    });
+  };
 
-        if (data.text.trim() === "") {
-          console.warn("No text was detected");
-        } else {
-          window.open(
-            `https://translate.google.com/?hl=en#auto/en/${data.text}`,
-            'GT',
-            "height=400,width=776,location=0,menubar=0,scrollbars=1,toolbar=0"
-          );
-        }
-      },
-      (e) => {
-        console.errror(e);
-      }
-    );
-};
-
-window.addEventListener("message", (e) => {
-  const image = document.getElementById("image");
-  image.src = e.data.dataUrl;
-  doOCR(e.data.dataUrl);
-});
+  initPage();
+}
